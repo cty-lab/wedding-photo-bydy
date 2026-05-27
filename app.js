758e63cf67729a97f975221f900a15bebfe6bba3
cty-lab <changtiffanycty@gmail.com>
2026-05-27 18:03:01 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fileInput = document.getElementById('file-input');
 const uploadStatus = document.getElementById('upload-status');
 const photoGrid = document.getElementById('photo-grid');
 
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbx4AgBveM1ToVjv5U08qhV3qnt4qngZGTCXZf1XvzrQxLru0jbumrSblRJ8IsxpBxWK/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwm7wu1rZS-2d8RSh6J9hS83ETHWp6mPE5cbgmwM1oIblS4AhoqxdnczibfdCiSwpj1ww/exec';
 
 fileInput.addEventListener('change', async (e) => {
   const files = e.target.files;
